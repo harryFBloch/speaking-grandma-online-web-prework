6cdeb98679b0b5ec01ce1262ecb8c9dcbca3cc60
Harry Bloch <harryfbloch@gmail.com>
2018-09-18 17:07:16 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,8 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(convo)
-  if convo == convo.t
-  if (convo == "I LOVE YOU GRANDMA!")
+  if (convo == convo.upcase)
+    return "HUH?! SPEAK UP, SONNY!"
+  elsif (convo == "I LOVE YOU GRANDMA!")
     return "I LOVE YOU TOO PUMPKIN!"
   end
 end
